076bcf9c57a29ee95fda81dc815bebfa3a8bccd7
Paul OHL <paul.lv.ohl@gmail.com>
2022-04-20 12:51:41 -0400

    Actions are now called on button click!

--- js/background.js
+++ js/background.js
@@ -4,3 +4,10 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({ invidiousInstance });
 	console.log('Default Invidious instance set to %cgreen', `invidiousInstance: ${invidiousInstance}`);
 });
+
+chrome.action.onClicked.addListener((tab) => {
+	chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		files: ['clickedButton.js']
+	});
+});
