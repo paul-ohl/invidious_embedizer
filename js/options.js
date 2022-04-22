a4dd2dda5543ce79f84ae5527190adff7d06f434
Paul OHL <paul.lv.ohl@gmail.com>
2022-04-22 08:58:59 -0400

    A more reactive save button

--- js/options.js
+++ js/options.js
@@ -1,16 +1,30 @@
 let invidiousInstanceInput = document.getElementById("invidious-instance");
 let autoRunInput = document.getElementById("autoRun");
+let saveSettingsButton = document.getElementById("saveSettingsButton");
 
 chrome.storage.sync.get(['invidiousInstance', 'autoRun'], function(result) {
 	invidiousInstanceInput.value = result.invidiousInstance;
 	autoRunInput.checked = result.autoRun;
 });
 
-document.getElementById("saveSettingsButton").addEventListener("click", () => {
+saveSettingsButton.addEventListener("click", () => {
+	saveSettingsButton.disabled = true;
+	saveSettingsButton.innerHTML = "...";
+
 	let invidiousInstance = invidiousInstanceInput.value;
 	let autoRun = autoRunInput.checked;
 	chrome.storage.sync.set({
 		invidiousInstance,
 		autoRun,
+	}, () => {
+		saveSettingsButton.innerHTML = "Saved!";
 	});
 });
+
+function reactivateButton() {
+	saveSettingsButton.innerHTML = "Save Settings";
+	saveSettingsButton.disabled = false;
+}
+
+invidiousInstanceInput.addEventListener("input", reactivateButton);
+autoRunInput.addEventListener("change", reactivateButton);
